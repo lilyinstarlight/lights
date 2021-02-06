74fe8bfb5ecc63aea62b6d728241e97f9e1fdd2a
Lily Foster <lily@lily.flowers>
2021-02-06 12:47:54 -0500

    add form parsing/emitting logic and add color picker form with styling

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,21 @@
 #[macro_use] extern crate rocket;
 
 use std::env;
+use std::fmt;
 use std::thread;
 
 use std::collections::HashMap;
 use std::net::UdpSocket;
+use std::num::ParseIntError;
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
 use std::sync::{Arc, Mutex};
 
 use rocket::State;
 use rocket::fairing::AdHoc;
-use rocket::http::Status;
+use rocket::http::{RawStr, Status};
+use rocket::request::{Form, FromFormValue};
+use rocket::response::NamedFile;
 
 use rocket_contrib::json::Json;
 use rocket_contrib::templates::Template;
@@ -32,6 +38,59 @@ struct Color {
     blue: u8,
 }
 
+enum ColorError {
+    BadFormat,
+    ParseError,
+}
+
+impl FromStr for Color {
+    type Err = ColorError;
+
+    fn from_str(color: &str) -> Result<Self, Self::Err> {
+        if &color[0..1] != "#" || color.len() != 7 {
+            return Err(Self::Err::BadFormat);
+        }
+
+        let result = || -> Result<Color, ParseIntError> {
+            let red = u8::from_str_radix(&color[1..3], 16)?;
+            let green = u8::from_str_radix(&color[3..5], 16)?;
+            let blue = u8::from_str_radix(&color[5..7], 16)?;
+
+            Ok(Color { red, green, blue })
+        }();
+
+        match result {
+            Err(_err) => {
+                return Err(Self::Err::ParseError);
+            },
+            Ok(color) => {
+                return Ok(color);
+            }
+        }
+    }
+}
+
+impl<'v> FromFormValue<'v> for Color {
+    type Error = ColorError;
+
+    fn from_form_value(value: &'v RawStr) -> Result<Self, Self::Error> {
+        match value.url_decode() {
+            Err(_err) => {
+                return Err(Self::Error::BadFormat);
+            },
+            Ok(color) => {
+                return Color::from_str(&color);
+            }
+        }
+    }
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "#{:02x}{:02x}{:02x}", self.red, self.green, self.blue)
+    }
+}
+
 #[derive(Clone, Serialize, Deserialize)]
 struct Error {
     status: String,
@@ -48,6 +107,11 @@ struct Output {
 }
 
 type CurrentOutput = Arc<Mutex<Output>>;
+
+#[derive(FromForm)]
+struct ColorForm {
+    color: Color,
+}
 
 #[get("/color")]
 fn get_color(current: State<CurrentColor>) -> Json<Color> {
@@ -68,14 +132,36 @@ fn set_color(color: Json<Color>, current: State<CurrentColor>, output: State<Cur
     Status::NoContent
 }
 
-#[get("/")]
-fn form() -> Template {
-    Template::render("form", HashMap::<String, String>::new())
+#[get("/static/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
 }
 
-#[post("/")]
-fn form_submit() -> Template {
-    form()
+#[get("/")]
+fn form(current: State<CurrentColor>) -> Template {
+    let current_color = current.lock().unwrap();
+
+    let context = [
+        (String::from("color"), current_color.to_string()),
+    ];
+
+    Template::render("form", context.iter().cloned().collect::<HashMap<String, String>>())
+}
+
+#[post("/", data = "<color_form>")]
+fn form_submit(color_form: Form<ColorForm>, current: State<CurrentColor>, output: State<CurrentOutput>) -> Template {
+    {
+        let mut current_color = current.lock().unwrap();
+        let mut current_output = output.lock().unwrap();
+
+        current_color.red = color_form.color.red;
+        current_color.green = color_form.color.green;
+        current_color.blue = color_form.color.blue;
+
+        set_output(&mut current_output, *current_color).unwrap();
+    }
+
+    form(current)
 }
 
 #[catch(400)]
@@ -212,7 +298,7 @@ fn main() {
     let osc_output = Arc::clone(&current_output);
 
     rocket::ignite()
-        .mount("/", routes![get_color, set_color, form, form_submit])
+        .mount("/", routes![get_color, set_color, files, form, form_submit])
         .register(catchers![bad_request, unprocessable_entity, not_found])
         .manage(rocket_color)
         .manage(rocket_output)
